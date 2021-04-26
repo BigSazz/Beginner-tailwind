2f62468d730db93fa30a2701cf0acb477621d6b3
Osaze Oviawe <osaze.o.oviawe@gmail.com>
2021-04-26 23:04:26 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: [
+    "./*.html"
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        orange: colors.orange,
+        teal: colors.teal,
+        lightBlue: colors.lightBlue
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
